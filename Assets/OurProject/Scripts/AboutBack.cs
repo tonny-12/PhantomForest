3aea38b2908b1cec0422edfd6464d1943168c014
aitorsahonero <sahonero.aitor@gmail.com>
2017-04-11 08:01:19 -0400

    Introduced title and about menus and level 1 rework

---
+++ Assets/OurProject/Scripts/AboutBack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AboutBack : MonoBehaviour {
+
+    public bool isBack;
+
+    void onMouseUp()
+    {
+        if (isBack)
+        {
+            SceneManager.LoadScene(0);
+            //Application.LoadLevel(0);
+        }
+
+    }
+}
