76f33c71cf6500a78039d0a1c5af9eecde58bd3c
Tonny <tonnyx1294@hotmail.com>
2017-04-19 06:31:51 -0400

    added boss

---
+++ Assets/OurProject/Scripts/MyBossHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBossHealth : MonoBehaviour {
+
+    public int bossMaxHealth;
+    public int bossCurrentHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        bossCurrentHealth = bossMaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bossCurrentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void HurtEnemy(int damageToGive)
+    {
+        bossCurrentHealth -= damageToGive;
+    }
+
+    public void SetMaxHealth()
+    {
+        bossCurrentHealth = bossMaxHealth;
+    }
+}
